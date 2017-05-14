f2993af10fc3fe7e061fee80581fc4e981c367e7
AoEiuV020 <aoeiuv020@gmail.com>
2017-05-14 15:11:56 +0800

    Apply kotlin

---
+++ lib/src/test/kotlin/KotlinTest.kt
@@ -0,0 +1,14 @@
+
+import org.junit.Test
+import kotlin.test.*
+
+/**
+ * Created by AoEiuV020 on 2017/05/14.
+ */
+class KotlinTest {
+    @Test
+    fun kotlinTest() {
+        assertEquals(2, 1 + 1)
+    }
+}
+
